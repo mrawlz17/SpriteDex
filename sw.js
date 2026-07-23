5d93adf7ce6332366a94dd5e68f90fafab4170c4
mrawlz17 <p8p84b9tc6@privaterelay.appleid.com>
2026-07-23 11:20:02 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='spritedex-v3-20260723';
+const ASSETS=['./','./index.html','./sprites.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request))));
